28fa4d89542901d6dc471e5834cf86fe4fc6282b
DannyAinge44 <david99.eggenberger@gmail.com>
2021-03-21 12:56:28 +0100

    Add Identity

--- MlsaAwesomeBlazor/Server/Startup.cs
+++ MlsaAwesomeBlazor/Server/Startup.cs
@@ -1,11 +1,16 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MlsaAwesomeBlazor.Server.Hubs;
+using MlsaAwesomeBlazor.Server.Identity;
 using System.Linq;
 
 namespace MlsaAwesomeBlazor.Server
@@ -23,10 +28,38 @@ namespace MlsaAwesomeBlazor.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
             services.AddSignalR();
             services.AddRazorPages();
+            services.AddDbContext<ApplicationDbContext>(op =>
+            {
+                op.UseInMemoryDatabase(databaseName: "IdentityDatabase");
+            });
+
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.User.RequireUniqueEmail = false;
+                options.SignIn.RequireConfirmedAccount = false;
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789, -._@+/ ";
+            }).AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<IdentityUser, ApplicationDbContext>();
+
+            services.AddAuthentication()
+                .AddGitHub(op =>
+                {
+                    op.Scope.Add("email");
+                    op.ClientId = "4aaea7373a832a79b9bb";
+                    op.ClientSecret = Configuration["GitHubId"];
+                })
+                .AddIdentityServerJwt();
+
+            services.ConfigureApplicationCookie(op =>
+            {
+                op.LogoutPath = "/Logout";
+                op.LoginPath = "/Login";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +82,10 @@ namespace MlsaAwesomeBlazor.Server
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseIdentityServer();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
